ba55ebac6247c547301a13f26793b0ffa33b7fb9
Akshit Mehta <157466686+BriskAM@users.noreply.github.com>
2024-10-20 02:08:50 +0530

    Add assignment_{0..2}

---
+++ assignment_1/script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget http://local.server/sample_1.txt -O s1.txt
+curl -o s2.txt http://local.server/sample_2.txt
+curl -o s3.txt http://local.server/sample_3.txt
+curl -o s4.txt http://local.server/sample_4.txt
---
+++ assignment_2/script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -T 23f3001694.txt http://local.server/upload/23f3001694.txt
