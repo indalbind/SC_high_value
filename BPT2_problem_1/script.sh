214e0a684bfa62d963a64604090b30d6c06ad4a6
Akshit Mehta <157466686+BriskAM@users.noreply.github.com>
2024-10-20 15:35:05 +0530

    Update BPT2_1

--- BPT2_problem_1/script.sh
+++ BPT2_problem_1/script.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-while IFS= read -r line; do
-    country=$(echo "$line" | grep -oP '(?<=<span class="country-name">)[^<]+')
-    capital=$(echo "$line" | grep -oP '(?<=<span class="country-capital">)[^<]+')
-
-    if [ -n "$country" ] && [ -n "$capital" ]; then
-        echo "$country"
-        echo "$capital"
-    fi
-done
+grep -oP '(?<=<span class="country-name">)[^<]+|(?<=<span class="country-capital">)[^<]+'
